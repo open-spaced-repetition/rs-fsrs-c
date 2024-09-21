2aa87f035069fb30c3fb89b43aea705eaade50f4
Asuka Minato <i@asukaminato.eu.org>
2024-09-21 18:28:50 +0900

    try set LD_LIBRARY_PATH

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -eux
 cargo build
-find target -name "librs_fsrs_c.so" -not -path "*/deps/*" -type f -print -exec gcc examples/basic.c {} \;
+find target \
+  -name "librs_fsrs_c.so" \
+  -not -path "*/deps/*" \
+  -type f -print \
+  -exec env LD_LIBRARY_PATH=target/debug/librs_fsrs_c.so gcc examples/basic.c {} \;
 ./a.out
