f9f614cc4f0d556692a73af372b9731e3c6d3ad4
Asuka Minato <i@asukaminato.eu.org>
2024-09-21 18:14:08 +0900

    see where is the so

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -eux
 cargo build
-gcc examples/basic.c target/debug/librs_fsrs_c.so
+find target -name "librs_fsrs_c*" -type f -print -exec gcc examples/basic.c target/debug/{} \;
 ./a.out
-
