56f3c061ad6eee5f9d76bf77e3cc2fed472b472e
Asuka Minato <i@asukaminato.eu.org>
2024-09-21 18:24:36 +0900

    try again

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eux
 cargo build
-find target -name "librs_fsrs_c.so" -type f -print -exec gcc examples/basic.c {} \;
+find target -name "librs_fsrs_c.so" -not -path "*/deps/*" -type f -print -exec gcc examples/basic.c {} \;
 ./a.out
