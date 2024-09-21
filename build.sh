ebc5b3bd01faf5605ac8c7a5b3e2eaef891743d2
Asuka Minato <i@asukaminato.eu.org>
2024-09-21 18:15:33 +0900

    fix name and path

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eux
 cargo build
-find target -name "librs_fsrs_c*" -type f -print -exec gcc examples/basic.c target/debug/{} \;
+find target -name "librs_fsrs_c.so" -type f -print -exec gcc examples/basic.c {} \;
 ./a.out
