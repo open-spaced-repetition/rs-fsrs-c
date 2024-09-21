fd6a2185b9d69af030d4614a894af713ebb0613b
Asuka Minato <i@asukaminato.eu.org>
2024-09-21 18:30:14 +0900

    see whether it works?

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ find target \
   -name "librs_fsrs_c.so" \
   -not -path "*/deps/*" \
   -type f -print \
-  -exec env LD_LIBRARY_PATH=target/debug/librs_fsrs_c.so gcc examples/basic.c {} \;
+  -exec env LD_LIBRARY_PATH=/home/runner/work/rs-fsrs-c/target/debug/librs_fsrs_c.so gcc examples/basic.c {} \;
 ./a.out
