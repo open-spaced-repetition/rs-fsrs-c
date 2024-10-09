734530fd9238a970c3850731820de194f67598b1
Asuka Minato <i@asukaminato.eu.org>
2024-10-10 03:53:13 +0900

    Update binding and all platform (#7)
    * to 1.0.0
    
    * fix Warnings
    
    * all platform

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -eux
 cargo build
 BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
-gcc -o a.out examples/basic.c -L${BASEDIR}/target/debug/ -lrs_fsrs_c -I${BASEDIR}/include/
+gcc -o a.out examples/basic.c -L${BASEDIR}/target/debug/ -lrs_fsrs_c -I${BASEDIR}/include/ -Wall -Wextra -pedantic
 env LD_LIBRARY_PATH=${BASEDIR}/target/debug/ ./a.out
--- examples/basic.c
+++ examples/basic.c
@@ -1,31 +1,47 @@
 #include "../include/fsrs.h"
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <time.h>
 
 int main(void) {
-  fsrs_Fsrs F1 = fsrs_Fsrs_new(
+  fsrs_Fsrs _F1 = fsrs_Fsrs_new(
       (fsrs_Parameters){.maximum_interval = 36000,
                         .request_retention = 0.9,
                         .w = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                              0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}});
+                              0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+                        .decay = 0.0,
+                        .factor = 0.0,
+                        .enable_short_term = false});
   fsrs_Card c = fsrs_Card_new();
   fsrs_Fsrs F = fsrs_Fsrs_default();
   time_t t = time(NULL);
 
-  fsrs_ScheduledCards scheduled_cards = fsrs_Fsrs_schedule_timestamp(&F, &c, t);
-  fsrs_Rating r = fsrs_Rating_Easy;
-  fsrs_Card updated_card = fsrs_ScheduledCards_select_card(&scheduled_cards, r);
-  fsrs_Option_ReviewLog option_log = fsrs_Card_log(&updated_card);
-  if (option_log.none) {
-    printf("No log\n");
+  fsrs_RecordLog scheduled_cards = fsrs_Fsrs_repeat_timestamp(&F, &c, t);
+  const fsrs_Rating ratings[] = {fsrs_Rating_Again, fsrs_Rating_Hard,
+                                 fsrs_Rating_Good, fsrs_Rating_Easy};
+  for (int i = 0; i < 4; i++) {
+    fsrs_Rating rating = ratings[i];
+    fsrs_SchedulingInfo scheduling_info =
+        fsrs_ScheduledCards_get(&scheduled_cards, rating);
+    fsrs_Card card = fsrs_SchedulingInfo_card(&scheduling_info);
+    fsrs_ReviewLog log = fsrs_SchedulingInfo_review_log(&scheduling_info);
+
+    printf("scheduled_days: %" PRId64 "\nelapsed_days: %" PRId64
+           "\ndate: %s"
+           "rating: %d"
+           "\nstate: %d"
+           "\n",
+           log.scheduled_days, log.elapsed_days,
+           asctime(localtime(&log.reviewed_date_s)), log.rating, log.state);
+    printf("card:\n  elapsed_days: %ld\n  scheduled_days: %ld\n  due: %ld\n  "
+           "stability: %f\n  difficulty: %f\n  reps: %d\n  lapses: %d\n  "
+           "state: %u\n  last_review: %ld\n",
+           fsrs_Card_elapsed_days(&card), fsrs_Card_scheduled_days(&card),
+           fsrs_Card_due(&card), fsrs_Card_stability(&card),
+           fsrs_Card_difficulty(&card), fsrs_Card_reps(&card),
+           fsrs_Card_lapses(&card), fsrs_Card_state(&card),
+           fsrs_Card_last_review(&card));
     return 0;
   }
-  fsrs_ReviewLog log = option_log.log;
-  printf("scheduled_days: %ld, elapsed_days: %ld, timestamp: %ld, rating: "
-         "%d, state: %d\n",
-         log.scheduled_days, log.elapsed_days, log.reviewed_date_s, log.rating,
-         log.state);
-  puts(asctime(localtime(&log.reviewed_date_s)));
-  return 0;
 }
