274b3c3f8d505610ff6b381a208e533ee0567af4
Asuka Minato <i@asukaminato.eu.org>
2024-10-24 02:47:35 +0900

    fix

--- build.sh
+++ build.sh
@@ -2,16 +2,15 @@
 set -eux -o pipefail
 
 cargo build
-gcc \
-    -o a.out \
-    -L$PWD/target/debug/ \
-    -lrs_fsrs_c \
-    -I$PWD/include/ \
-    -Wall -Wextra -pedantic -fprofile-arcs -ftest-coverage \
-    examples/basic.c
-env LD_LIBRARY_PATH=$PWD/target/debug/ ./a.out
-
-if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    env LD_LIBRARY_PATH=$PWD/target/debug/ gcov a-basic.gcno a-basic.gcda
+BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
+gcc -o a.out examples/basic.c -L${BASEDIR}/target/debug/ -lrs_fsrs_c -I${BASEDIR}/include/ -Wall -Wextra -pedantic  -fprofile-arcs -ftest-coverage
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    cp target/debug/rs_fsrs_c.dll .
+    ./a.out
+    gcov *.gcno *.gcda
     cat basic.c.gcov
+else
+    LD_LIBRARY_PATH=${BASEDIR}/target/debug/ ./a.out
+    LD_LIBRARY_PATH=${BASEDIR}/target/debug/ gcov *.gcno *.gcda
+    cat *.gcov
 fi
