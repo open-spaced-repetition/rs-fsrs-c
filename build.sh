7b4bf1c674ababe992ec322553d133c64023b3ba
Asuka Minato <i@asukaminato.eu.org>
2024-10-24 01:48:52 +0900

    path

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 set -eux -o pipefail
 
 cargo build
-
+mv examples/basic.c .
 gcc \
     -o a.out \
     -L$PWD/target/debug/ \
     -lrs_fsrs_c \
     -I$PWD/include/ \
     -Wall -Wextra -pedantic -fprofile-arcs -ftest-coverage \
-    examples/basic.c
+    basic.c
 env LD_LIBRARY_PATH=$PWD/target/debug/ ./a.out
-gcov examples/basic.c
-cat examples/basic.c.gcov
+gcov basic.c
+cat basic.c.gcov
